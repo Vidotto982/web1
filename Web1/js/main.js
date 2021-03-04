97cd316f1cd7d2497e9ed197e4e6e0f0086eb2a0
Francisco Leonel Vidotto <vidotto982@gmail.com>
2021-03-04 16:46:52 -0300

    Listado de todos los nombres.

--- Web1/js/main.js
+++ Web1/js/main.js
@@ -4,10 +4,10 @@ let onePoke = document.getElementById("onePoke");
 
 function getTest() {
     let idPoke = document.getElementById("idPost").value;
-    const getDataAsync = async(idpoke) => {
+    const getDataAsync = async(idPoke) => {
         try {
             if (idPoke >= 1 & idPoke < 899) {
-                const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idpoke}`);
+                const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
                 const poke = await resPoke.json();
                 console.log(poke);
                 // const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.allPokeId}`)
@@ -15,20 +15,22 @@ function getTest() {
                 onePoke.innerHTML = poke.name;
             }
             if (idPoke < 1 || idPoke == '') {
+                idPoke = 1;
                 console.log("If 2")
-                const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idpoke}`);
+                const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
                 const poke = await resPoke.json();
-                for (let i = 0; i < poke.id; i++) {
-                    console.log("for")
-                    const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.id}`)
+                for (let i = 0; i < poke.id; poke.id++) {
+                    console.log("for" + " " + poke.name);
+                    const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.id}`);
                     const allPoke = await resAllPoke.json();
 
                     function createP() {
                         let divAll = document.getElementById("allPoke");
                         let p = document.createElement("p");
-                        p.innerHTML = allPoke;
+                        p.innerHTML = allPoke.name;
                         divAll.appendChild(p)
                     }
+                    createP();
                 }
             }
         } catch (error) {
