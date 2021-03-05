90bed39626928b2c0e1a41abb95bf71299eb4186
Francisco Leonel Vidotto <vidotto982@gmail.com>
2021-03-05 14:32:05 -0300

    separando funciones

--- Web1/js/main.js
+++ Web1/js/main.js
@@ -1,9 +1,10 @@
-let butTest = document.getElementById("sumbit").addEventListener("click", getTest);
-// let allPoke = document.getElementById("allPoke");
+let butOnePoke = document.getElementById("sumbit").addEventListener("click", getTest);
+let butAllPoke = document.getElementById("fullList").addEventListener("click", getAllPoke);
+
 let onePoke = document.getElementById("onePoke");
+let idPoke = document.getElementById("idPost").value;
 
 function getTest() {
-    let idPoke = document.getElementById("idPost").value;
     const getDataAsync = async(idPoke) => {
         try {
             if (idPoke >= 1 & idPoke < 899) {
@@ -14,29 +15,58 @@ function getTest() {
                 // const allPoke = await resAllPoke.json();
                 onePoke.innerHTML = poke.name;
             }
-            if (idPoke < 1 || idPoke == '') {
-                idPoke = 1;
-                console.log("If 2")
-                const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
-                const poke = await resPoke.json();
-                for (let i = 0; i < poke.id; poke.id++) {
-                    console.log("for" + " " + poke.name);
-                    const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.id}`);
-                    const allPoke = await resAllPoke.json();
+            // if (idPoke < 1 || idPoke == '') {
+            //     idPoke = 1;
+            //     console.log("If 2")
+            //     const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
+            //     const poke = await resPoke.json();
+            //     for (let i = 0; i < poke.id; poke.id++) {
+            //         console.log("for" + " " + poke.name);
+            //         const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.id}`);
+            //         const allPoke = await resAllPoke.json();
 
-                    function createP() {
-                        let divAll = document.getElementById("allPoke");
-                        let p = document.createElement("p");
-                        p.innerHTML = allPoke.name;
-                        divAll.appendChild(p)
-                    }
-                    createP();
-                }
-            }
+            //         function createP() {
+            //             let divAll = document.getElementById("allPoke");
+            //             let p = document.createElement("p");
+            //             p.innerHTML = allPoke.name;
+            //             divAll.appendChild(p)
+            //         }
+            //         createP();
+            //     }
+            // }
         } catch (error) {
             console.log("error" + " " + error);
         }
 
     }
     getDataAsync(idPoke);
+}
+
+function getAllPoke() {
+    const getDataAsync = async(idPoke) => {
+        try {
+            idPoke = 1;
+            console.log("If 2")
+            const resPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${idPoke}`);
+            const poke = await resPoke.json();
+            for (let i = 0; i < poke.id; poke.id++) {
+                console.log("for" + " " + poke.name);
+                const resAllPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.id}`);
+                const allPoke = await resAllPoke.json();
+
+                function createP() {
+                    let divAll = document.getElementById("allPoke");
+                    let p = document.createElement("p");
+                    p.innerHTML = allPoke.name;
+                    divAll.appendChild(p)
+                }
+                createP();
+            }
+
+        } catch (error) {
+            console.log("error" + " " + error);
+        }
+
+    }
+    getDataAsync(idPoke)
 }
